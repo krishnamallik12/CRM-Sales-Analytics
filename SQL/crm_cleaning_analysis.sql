d9e7d302352109f4c5c45b37507c98d92f4999e2
krishnamallik12 <krishnamallik1200@gmail.com>
2026-08-24 00:28:15 +0530

    Update crm_cleaning_analysis.sql

--- SQL/crm_cleaning_analysis.sql
+++ SQL/crm_cleaning_analysis.sql
@@ -58,8 +58,12 @@ LIMIT 1000;
 -- ------------------------------------------------------------------------------------
 
 -- DATA CLEANING
--- Check for Duplicates
+-- checking data types
+desc accounts_staging;
 
+-- Check for Duplicates
+update accounts_staging
+set account = trim(account);
       
 select account, sector, year_established, revenue, employees, office_location, subsidiary_of,count(*)
 from accounts_staging
@@ -97,29 +101,27 @@ where revenue<=0;
 
 select account
 from accounts_staging
-where year_established>= curdate();
+where year_established>= year(curdate());
 
 -- Standardizing sector names
-  
+select distinct sector from accounts_staging;
 update accounts_staging
 set sector = "technology"
 where sector = "technolgy";
 
-update accounts_staging
-set account = trim(account);
 
 
 
 -- PRODUCTS_STAGING TABLE
-
-select * from products_staging;
+desc products_staging;
+select * from products_staging limit 100;
 
 update products_staging
 set product = trim(product);
 
 -- Check for duplicate products
-select product
-from (select product, row_number() over(partition by product,series) as row_num
+select product,series,sales_price
+from (select product,series,sales_price, row_number() over(partition by product,series) as row_num
       from products_staging) product_duplicate
 where row_num>1;
 
@@ -138,21 +140,21 @@ from products_staging;
 
 
 -- SALES_TEAMS_STAGING TABLE
-
-select * from sales_teams_staging;
+desc sales_teams_staging;
+select * from sales_teams_staging limit 100;
 
 -- Check if one agent works under multiple manager
-select sales_agent, count(manager) 
+select sales_agent, count(distinct manager) 
 from sales_teams_staging
 group by sales_agent
-having count(manager)>1;
+having count(distinct manager)>1;
 
 select distinct regional_office
 from sales_teams_staging;
 
 
 -- SALES_PIPELINE_STAGING
-select * from sales_pipeline_staging;
+select * from sales_pipeline_staging limit 100;
 
 -- checking datatypes
 describe sales_pipeline_staging;
@@ -189,9 +191,9 @@ where trim(account) = '' or account is NULL;
 -- Specific agents
 -- Specific product
 
-select deal_stage, count(*)
+select deal_stage, count(*) as no_account
 from sales_pipeline_staging
-where account is NULL 
+where account is NULL or account=''
 group by deal_stage;
 
 -- Since all NULL accounts are in early stages:
@@ -208,7 +210,7 @@ add column has_account tinyint;
 update sales_pipeline_staging
 set has_account =
     case
-		when account is NULL then 0
+		when account is NULL or account='' then 0
         else 1
 	end ;
         
@@ -217,7 +219,7 @@ select has_account,count(*)
 from sales_pipeline_staging
 group by has_account;
 
- select count(opportunity_id) 
+select count(opportunity_id) 
 from sales_pipeline_staging
 where trim(sales_agent) = '' or sales_agent is null;
 
@@ -247,12 +249,16 @@ where deal_stage = "won" and (close_value is NULL or close_value = 0);
 
 -- Check Exact Duplicates (Make sure the system didn’t accidentally duplicate rows)
 
-select opportunity_id,sales_agent,product,account,deal_stage,engage_date,close_date,close_value,count(*)
+-- Exact duplicate check
+select opportunity_id, sales_agent, product, account, deal_stage,
+       engage_date, close_date, close_value, COUNT(*) as duplicate_count
 from sales_pipeline_staging
-group by opportunity_id,sales_agent,product,account,deal_stage,engage_date,close_date,close_value
-having count(*)>1;
+group by opportunity_id, sales_agent, product, account, deal_stage,
+         engage_date, close_date, close_value
+having COUNT(*) > 1;
 -- returned 0 rows , meaning no exact system duplication
 
+-- Potentially duplicated opportunity IDs
 select opportunity_id, count(*)
 from sales_pipeline_staging
 group by opportunity_id
@@ -273,6 +279,7 @@ having count(*)>1;
    --------------------------- */
 
 /* Step 1: Identify invalid or non-convertible date values */
+-- Identify values that match none of the supported formats
 select engage_date
 from sales_pipeline_staging
 where engage_date is not null
@@ -347,14 +354,42 @@ modify close_date date;
    --------------------------- */
 
 /* Ensure no opportunity closes before it is engaged */
-SELECT opportunity_id
-FROM sales_pipeline_staging
-WHERE close_date < engage_date;
+select opportunity_id
+from sales_pipeline_staging
+where close_date < engage_date;
 
 
 select count(*)
 from sales_pipeline_staging
 where deal_stage IN ("won", "lost") and close_value is NULL;
+
+-- Referential-Integrity checks 
+-- Accounts not found in master table
+select distinct sp.account
+from sales_pipeline_staging sp
+left join accounts_staging a
+    on sp.account = a.account
+where sp.account is not NULL
+  and trim(sp.account) <> ''
+  and a.account is NULL;
+
+-- Products not found in master table
+select distinct sp.product
+FROM sales_pipeline_staging sp
+left join products_staging p
+    on sp.product = p.product
+where sp.product is not NULL
+  and TRIM(sp.product) <> ''
+  and p.product is NULL;
+
+-- Sales agents not found in sales team master
+select distinct sp.sales_agent
+from sales_pipeline_staging sp
+left join sales_teams_staging st
+    on sp.sales_agent = st.sales_agent
+where sp.sales_agent is not NULL
+  and TRIM(sp.sales_agent) <> ''
+  and st.sales_agent is NULL;
 
 update sales_pipeline_staging sp
 left join products_staging p
@@ -376,126 +411,6 @@ select count(*)
 from sales_pipeline_staging
 where deal_stage = "lost" and close_value <> 0 ;
 
-
--- Questions
-
--- Ques 1
--- How does closed-won revenue for each product compare between the most recent quarter and the previous quarter, and what is the percentage change?
--- Quarter-over-Quarter (QoQ) Revenue by Product
-
-with QuarterlySales as(
-	select product, 
-	year(close_date) as sales_year, 
-	quarter(close_date) as sales_quarter, 
-	sum(close_value) as current_revenue
-	from sales_pipeline_staging
-	where deal_stage = "won" and close_date is not NULL
-	group by product, year(close_date), quarter(close_date)
-)
-,QoQ_comparision as(
-	select product, sales_year, sales_quarter,current_revenue,
-	lag(current_revenue) over(partition by product order by sales_year,sales_quarter) as prev_quarter_revenue
-	from QuarterlySales
-)
-select product,
-sales_year,
-sales_quarter,
-current_revenue,
-prev_quarter_revenue,
-concat(round(((current_revenue - prev_quarter_revenue)/prev_quarter_revenue)*100,2),'','%') as percent_change from QoQ_comparision qc
-;
-
--- Ques 2
--- Agent-level analysis:
--- Which sales agents generated the highest closed-won revenue
--- what does their average deal duration compare to the overall average?
-
-with company_avg as (
-	select avg(datediff(close_date,engage_date)) as company_avg_deal_duration
-	from sales_pipeline_staging
-	where deal_stage = "won" 
-		  and close_date is not NULL
-		  and engage_date is not null
-)
-select sales_agent,
-sum(close_value) as total_revenue_generated, 
-round(avg(datediff(close_date,engage_date)),2) as agent_avg_deal_duration,
-round((select company_avg_deal_duration from company_avg),2) as company_avg_deal_duration
-from sales_pipeline_staging
-where deal_stage = "won" and close_value is not null
-group by sales_agent
-order by sum(close_value) desc
-;
-
--- Ques 3
--- Which lean, capital-efficient accounts (below avg headcount but above avg company revenue) generate the highest closed-won revenue?
---    These accounts have high revenue-per-employee ratios, suggesting
---    streamlined decision-making and stronger purchasing power.
---    Isdom, Codehow and Gekko & Co lead this segment 
---    These accounts should be prioritized for upselling and retention given their
---    demonstrated spending capacity relative to their team size.
-with benchmarks as(
-	select avg(employees) as avg_employees, 
-	avg(revenue) as avg_revenue
-	from accounts_staging)
-select sp.account, 
-a.revenue as revenue_in_millions,
-a.employees, 
-sum(sp.close_value) as total_sales,
-round(sum(sp.close_value)/(a.employees),2) as sales_per_employee
-from sales_pipeline_staging sp
-join accounts_staging a
-	on sp.account = a.account
-cross join benchmarks
-where a.employees< benchmarks.avg_employees
-      and a.revenue> benchmarks.avg_revenue
-      and sp.deal_stage ="won"
-group by sp.account,a.employees, a.revenue 
-order by sum(sp.close_value) desc;
-
-
--- Ques 4
--- Which accounts generate the most deal volume vs actual revenue?
---    Identifies accounts consuming disproportionate sales effort 
---    relative to their revenue contribution
-select account,
-count(case when deal_stage='won' then 1 end) as Total_won_deals,
-count(case when deal_stage='lost' then 1 end) as Total_lost_deals,
-sum(case when deal_stage= 'won' then close_value else 0 end) as Total_revenue,
-round(sum(case when deal_stage= 'won' then close_value else 0 end)/nullif(count(case when deal_stage='won' then 1 end),0),2) as revenue_per_won_deal,
-round((count(case when deal_stage='won' then 1 end) / 
-	nullif(count(case when deal_stage in ('won','lost') then 1 end),0))*100,2) as win_rate
-from sales_pipeline_staging
-where account is not NULL
-group by account
-order by win_rate desc;
-
-
--- Ques 5
--- How long does it typically take for an unsuccessful deal to drop off after initial engagement?
-with categorized_loss as(
-	select opportunity_id,
-    case
-		when datediff(close_date,engage_date)<=2 then 'Instant Rejection (0-2 Days)'
-        when datediff(close_date,engage_date) between 3 and 30 then 'Short Engagement (3-30 Days)'
-        else 'Prolonged Disengagement (30+ Days)'
-	end as drop_off_category
-    from sales_pipeline_staging
-    where deal_stage = 'lost')
-select drop_off_category, count(opportunity_id) as Total_lost_deals,
-round((count(opportunity_id)/sum(count(opportunity_id)) over())*100,2) as Lost_Percent
-from categorized_loss 
-group by drop_off_category;
-
--- The data shows that our leads are highly qualified—only 11% instantly reject us. 
--- However, our sales team is bleeding time and resources. 
--- Nearly half of all our lost deals are 'Prolonged Disengagements,' meaning our reps are spending over a month
--- doing demos and negotiations only for the client to ghost them. 
--- We don't have a lead-generation problem; we have a closing problem.
-
--- Ques 6
--- Which regions generate the highest closed-won revenue per opportunity?
-
 -- checking if any agent is have more than one manager
 select * 
 from sales_teams_staging a
@@ -504,41 +419,178 @@ on a.sales_agent = b.sales_agent
 where a.manager <> b.manager;
 -- It returned no rows hence every agent has one manager
 
-select regional_office,
-round(sum(case when deal_stage = 'won' then close_value else 0 end)/count(opportunity_id),2) as won_revenue_per_opportunity
+
+-- Questions
+
+-- Ques 1
+
+-- How does closed-won revenue for each product change quarter-over-quarter in 2017, and what is the percentage change?
+-- Quarter-over-Quarter (QoQ) Revenue by Product
+
+with QuarterlySales as(
+	select product, 
+	year(close_date) as sales_year, 
+	quarter(close_date) as sales_quarter, 
+	sum(close_value) as quarterly_revenue
+	from sales_pipeline_staging
+	where deal_stage = "won" and close_date is not NULL
+	group by product, year(close_date), quarter(close_date)
+)
+,QoQ_comparision as(
+	select product, sales_year, sales_quarter,quarterly_revenue,
+	lag(quarterly_revenue) over(partition by product order by sales_year,sales_quarter) as prev_quarter_revenue
+	from QuarterlySales
+)
+select product,
+sales_year,
+sales_quarter,
+quarterly_revenue,
+prev_quarter_revenue,
+concat(round(((quarterly_revenue - prev_quarter_revenue)/nullif(prev_quarter_revenue,0))*100,2),'%') as percent_change from QoQ_comparision 
+;
+
+-- Ques 2
+-- Agent-level analysis:
+-- Which sales agents generated the highest closed-won revenue, and how does their average deal duration compare with the overall average?
+
+with overall_avg as (
+	select avg(datediff(close_date,engage_date)) as overall_avg_deal_duration
+	from sales_pipeline_staging
+	where deal_stage = "won" 
+		  and close_value is not NULL
+		  and close_date is not NULL
+		  and engage_date is not null
+)
+select sales_agent,
+sum(close_value) as total_revenue_generated, 
+count(*) as won_deals,
+round(avg(datediff(close_date,engage_date)),2) as agent_avg_deal_duration,
+round((select overall_avg_deal_duration from overall_avg),2) as overall_avg_deal_duration,
+round((avg(datediff(close_date,engage_date)) - (select overall_avg_deal_duration from overall_avg)),2) as difference_from_company_avg
+from sales_pipeline_staging
+where deal_stage = "won" 
+  and close_value is not NULL
+  and close_date is not NULL
+  and engage_date is not NULL
+group by sales_agent
+order by total_revenue_generated desc
+;
+
+-- Sales Agent: Darcel Schlecht generated the highest revenue and closed the deals 2 days faster than the overall average. 
+
+-- Ques 3
+-- Which sectors generate the highest closed-won revenue, and how do their win rates, deal volumes, and average won deal values compare?
+
+select 
+    a.sector,
+    sum(case when sp.deal_stage = 'won' then sp.close_value else 0 end) as closed_won_revenue,
+    count(case when sp.deal_stage = 'won' then 1 end) as won_deals,
+    round(count(case when sp.deal_stage = 'won' then 1 end)/nullif(count(case when sp.deal_stage in ('won','lost') then 1 end),0) * 100,2) as win_rate_percentage,
+    round(sum(case when sp.deal_stage = 'won' then sp.close_value else 0 end)/nullif(count(case when sp.deal_stage = 'won' then 1 end),0),2) as avg_won_deal_value
+from sales_pipeline_staging sp
+inner join accounts_staging a
+    on sp.account = a.account
+where sp.account is not null
+group by a.sector
+order by closed_won_revenue desc;
+
+-- Ques 4
+-- Which accounts generate the most closed-won revenue, and how does their win rate compare?
+
+select account,
+count(case when deal_stage = 'won' then 1 end) as total_won_deals,
+sum(case when deal_stage = 'won' then close_value else 0 end) as total_won_revenue,
+round((count(case when deal_stage = 'won' then 1 end) / 
+    nullif(count(case when deal_stage in ('won','lost') then 1 end),0))*100,2) as win_rate
+from sales_pipeline_staging
+where account is not null
+group by account
+order by total_won_revenue desc;
+
+
+
+-- Ques 5
+-- How long do lost opportunities typically remain in the sales process after entering the engaging stage?
+with categorized_loss as(
+	select opportunity_id,
+    case
+		when datediff(close_date,engage_date)<=2 then 'Early Drop-off (0-2 Days)'
+        when datediff(close_date,engage_date) between 3 and 30 then 'Short Sales Cycle (3-30 Days)'
+        else 'Long Sales Cycle (31+ Days)'
+	end as drop_off_category
+    from sales_pipeline_staging
+    where deal_stage = 'lost'
+    and close_date is not null
+    and engage_date is not null)
+select drop_off_category, count(*) as Total_lost_deals,
+round((count(*)/sum(count(*)) over())*100,2) as Lost_Percent
+from categorized_loss 
+group by drop_off_category
+order by 
+case drop_off_category
+    when 'Early Drop-off (0-2 Days)' then 1
+    when 'Short Sales Cycle (3-30 Days)' then 2
+    when 'Long Sales Cycle (31+ Days)' then 3
+end;
+
+-- Only about 11.4% of Lost opportunities were closed within the first two days after engagement. 
+-- A significant share of Lost opportunities remain open for more than 30 days, which may indicate an opportunity to improve qualification or earlier disengagement.
+-- The largest category was Lost opportunities that remained open for more than 30 days, representing about 45.6% of all Lost opportunities.
+-- This suggests that a substantial proportion of opportunities are remaining active for a relatively long period before ultimately being Lost.
+
+-- Ques 6
+-- How does regional pipeline performance compare in terms of Won/Lost deals, closed-won revenue, and closed-won revenue generated per opportunity?
+
+
+select *
+from sales_teams_staging a, sales_teams_staging b
+where a.sales_agent=b.sales_agent
+and a.regional_office <> b.regional_office;
+
+select 
+    st.regional_office,
+    sum(case when s.deal_stage = 'won' then 1 else 0 end) as won_deals,
+    sum(case when s.deal_stage = 'lost' then 1 else 0 end) as lost_deals,
+    round(sum(case when s.deal_stage = 'won' then s.close_value else 0 end),2) as total_won_revenue,
+    round(sum(case when s.deal_stage = 'won' then s.close_value else 0 end)
+    /nullif(count(case when s.deal_stage in ('won','lost') then 1 end),0),2) as won_revenue_per_completed_opportunity
 from sales_pipeline_staging s
 join sales_teams_staging st
-on s.sales_agent = st.sales_agent 
+    on s.sales_agent = st.sales_agent
 group by st.regional_office
-order by won_revenue_per_opportunity desc;
+order by won_revenue_per_completed_opportunity desc;
 
 -- Ques 7
--- Which products close fastest on average, and how does deal duration relate to closed-won revenue?
+-- How do products differ in closed-won revenue, deal volume, deal value, and average closing time?
 
-select product,
-sum(close_value) as total_revenue,
-round(avg(datediff(close_date,engage_date)),2) as close_time_average
+select 
+    product,
+    sum(close_value) as total_won_revenue,
+    count(*) as won_deals,
+    round(avg(close_value),2) as avg_won_deal_value,
+    round(avg(datediff(close_date,engage_date)),2) as avg_close_time
 from sales_pipeline_staging
 where deal_stage = 'won'
+  and close_date is not null
+  and engage_date is not null
 group by product
-order by total_revenue desc;
--- GTK 500 was identified as a major efficiency bottleneck, 
--- taking a portfolio-high 64 days to close while yielding bottom-tier total revenue.
+order by total_won_revenue desc;
 
 -- Ques 8
 -- Manager-level analysis:
 -- How does sales performance vary by manager in terms of win rate and closed-won revenue across regions?
--- Evaluates team-level win rates and revenue across geographic offices
 
-select st.manager, sum(case when s.deal_stage = 'won' then s.close_value else 0 end) as total_revenue,
-st.regional_office, 
-round((count(case when s.deal_stage = 'won' then 1 end)/count(case when s.deal_stage in ('won','lost') then 1 end))*100,2) as win_rate
+select 
+    st.manager,
+    st.regional_office,
+    sum(case when s.deal_stage = 'won' then s.close_value else 0 end) as total_won_revenue,
+    count(case when s.deal_stage = 'won' then 1 end) as won_deals,
+    count(case when s.deal_stage in ('won','lost') then 1 end) as completed_deals,
+    round(count(case when s.deal_stage = 'won' then 1 end)/nullif(count(case when s.deal_stage in ('won','lost') then 1 end),0) * 100,2) as win_rate
 from sales_pipeline_staging s
 left join sales_teams_staging st
-on s.sales_agent = st.sales_agent
-group by st.manager,st.regional_office
-order by win_rate desc;
-
-
+    on s.sales_agent = st.sales_agent
+group by st.manager, st.regional_office
+order by total_won_revenue desc;
 
 
